1cf99c1b6195b2ceefb4de83aedbba08a9fa8103
Elvira <esinyanskaya@gmail.com>
2022-11-09 23:51:18 +0200

    Adding rating stars

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(".my-rating").starRating({
+  starSize: 25,
+  initialRating: 4.5,
+  readOnly: true,
+  useGradient: false,
+  starShape: 'rounded',
+  emptyColor: '#D7D1C7',
+  activeColor: '#FFB648'
+
+});
