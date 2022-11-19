7828c586c3280fbb119d80dc2660f31e91bdc5c6
Elvira <esinyanskaya@gmail.com>
2022-11-19 10:32:24 +0200

    Adding video

--- js/app.js
+++ js/app.js
@@ -1,45 +1,47 @@
-$(".header__rating-stars").starRating({
-  starSize: 36,
-  initialRating: 4.5,
-  readOnly: true,
-  useGradient: false,
-  starShape: 'rounded',
-  emptyColor: '#D7D1C7',
-  activeColor: '#FFB648'
+$(function () {
 
-});
+  $(".header__rating-stars").starRating({
+    starSize: 36,
+    initialRating: 4.5,
+    readOnly: true,
+    useGradient: false,
+    starShape: 'rounded',
+    emptyColor: '#D7D1C7',
+    activeColor: '#FFB648'
 
-var mixer = mixitup('.blog__list');
+  });
 
-$(document).on('click', '.blog__filter-btn', function () {
-  $('.blog__filter-btn').removeClass('blog__filter-btn--active')
-  $(this).addClass('blog__filter-btn--active');
+  var mixer = mixitup('.blog__list');
 
-})
+  $(document).on('click', '.blog__filter-btn', function () {
+    $('.blog__filter-btn').removeClass('blog__filter-btn--active')
+    $(this).addClass('blog__filter-btn--active');
 
-$('.customers__slider').slick({
-  infinite: false,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  dots: true,
-  arrows:false,
-  waitForAnimate: false,
-  appendDots: $(".customers__dots")
-});
+  })
 
-$('.customers__slider-prev').on('click', function (e) {
-  e.preventDefault()
-  $('.customers__slider').slick('slickPrev')
-})
-$('.customers__slider-next').on('click', function (e) {
-  e.preventDefault()
-  $('.customers__slider').slick('slickNext')
-})
+  $('.customers__slider').slick({
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    waitForAnimate: false,
+    appendDots: $(".customers__dots")
+  });
+
+  $('.customers__slider-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.customers__slider').slick('slickPrev')
+  })
+  $('.customers__slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.customers__slider').slick('slickNext')
+  })
 
 
-// accordion function
+  // accordion function
 
-$('.questions__accordion-link').on('click', function (e) {
+  $('.questions__accordion-link').on('click', function (e) {
     e.preventDefault()
     $(this).toggleClass('questions__accordion-item--active')
     $(this).children('.questions__accordion-text').slideToggle()
@@ -47,12 +49,12 @@ $('.questions__accordion-link').on('click', function (e) {
 
 
 
-
+})
   //======================================
-// -----> CHECK OVERFLOW <--------- 
+// -----> CHECK OVERFLOW <---------
 //
 //   var docWidth = document.documentElement.offsetWidth;
-// 
+//
 // [].forEach.call(
 //   document.querySelectorAll('*'),
 //   function(el) {
@@ -61,3 +63,4 @@ $('.questions__accordion-link').on('click', function (e) {
 //     }
 //   }
 // );
+
